212b286a921f0cbdbe917ef6bf725d1db0e71bd1
Kenon Willis <kwillis@lvjusd.k12.ca.us>
2018-09-20 10:05:49 -0700

    added if else

---
+++ If else/IfElse.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class IfElse here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class IfElse
+{
+   public static void main(){
+       
+       System.out.println("hi");
+    }
+}
---
+++ If else/NullorZero.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class NullorZero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NullorZero
+{
+   public static void main(){
+    
+    // instance variables - replace the example below with your own
+    String[] names = new String[6];
+    
+    for(int i=0; i<names.length; i++){
+        System.out.println(names[i]);
+    }
+    
+    int[] names1 = new int[6];
+    
+    for(int i=0; i<names.length; i++){
+        System.out.println(names1[i]);
+    }
+    
+}
+}
